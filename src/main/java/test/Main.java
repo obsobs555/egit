09827354daf10370acfd24ad0b78e680f0178ab3
Administrator <Administrator@administraion>
2017-12-29 10:15:27 +0800

    第一次修改

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Å·°ÍÉ£");
+		System.out.println("你好");
 		System.out.println("欧巴桑");
 	}
 

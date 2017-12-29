a460b25dc5b199abe0bb2aeb12aa6b6edb6910d4
Obs <34908787+obsobs555@users.noreply.github.com>
2017-12-29 10:13:23 +0800

    Update Main.java

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -7,7 +7,8 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ŷ��ɣ");
+		System.out.println("Å·°ÍÉ£");
+		System.out.println("欧巴桑");
 	}
 
 }
